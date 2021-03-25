e9955bc571e4fd8f77d1c64afd4154f42f6d9a63
jxwleong <jason.xie.wei.leong@gmail.com>
2021-03-25 21:03:05 +0800

    Added basic functions to create the process and logging Currently have issue with killing the process with PID either using os.kill() os psutil.

---
+++ image_path.py
@@ -0,0 +1,8 @@
+import os
+
+IMAGE_PATH = os.path.join(os.path.dirname(__file__), 'images')
+
+stress_test_image_path = {
+    'stress_unselected' : os.path.join(IMAGE_PATH, 'stress_test_unselected.PNG'),
+    'stress_selected' : os.path.join(IMAGE_PATH, 'stress_test_selected.PNG')
+}
---
+++ logger/log_config.py
@@ -0,0 +1,54 @@
+import logging.config
+import os
+from log_colorformatter import ColorFormatter
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+LOG_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'minimal': {
+            'format': '%(asctime)s:%(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+        'verbose': {
+            'format': '%(asctime)s:%(levelname)s:%(filename)s:%(lineno)d:%(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+        'color_format': {
+            'class': 'logging.ColorFormatter',
+            'format': '$TIME_COLOR%(asctime)s$RESET:$COLOR%(levelname)s$RESET:$WHITE%(filename)s:%(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'formatter': 'color_format',
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'formatter': 'verbose',
+            'filename': os.path.join(ROOT_DIR, os.path.basename(ROOT_DIR).split('.')[0] + '.log'),
+            'mode': 'w',
+            'class': 'logging.FileHandler',
+        },
+    },
+    'loggers': {
+        '': { # Root
+            'level': 'WARNING',
+            'handlers': ['console', 'file'],
+            'propagate': False,
+        },
+        '__main__': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file'],
+            'propagate': False,
+        },
+    },
+}
+
+
