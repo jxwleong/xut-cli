64fd01f6b92d79ccf7031b135617ae208fb90722
jxwleong <jason.xie.wei.leong@gmail.com>
2021-03-27 14:58:38 +0800

    Removed unused prototypes

--- image_path.py
+++
@@ -1,8 +0,0 @@
-import os
-
-IMAGE_PATH = os.path.join(os.path.dirname(__file__), 'images')
-
-stress_test_image_path = {
-    'stress_unselected' : os.path.join(IMAGE_PATH, 'stress_test_unselected.PNG'),
-    'stress_selected' : os.path.join(IMAGE_PATH, 'stress_test_selected.PNG')
-}
