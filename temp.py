18b90b6734984c99ffc147bf31f4a3ab3ef37f57
jxwleong <jason.xie.wei.leong@gmail.com>
2021-06-13 19:43:02 +0800

    Added some prototype code for using UIAutomation (https://github.com/yinkaisheng/Python-UIAutomation-for-Windows) Use UIASpy to get the relevent information. Get from https://www.autoitscript.com/forum/topic/196833-uiaspy-ui-automation-spy-tool/.

---
+++ temp.py
@@ -0,0 +1,53 @@
+import subprocess
+import uiautomation as auto
+auto.uiautomation.SetGlobalSearchTimeout(15)  # set new timeout 15
+
+xtu_exe = r"C:\Program Files\Intel\Intel(R) Extreme Tuning Utility\Client\XtuUiLauncher.exe"
+prime95=r"G:\MyProjects\xprime95\p95v303b6.win32\prime95.exe"
+def main():
+    subprocess.Popen(xtu_exe)
+    window = auto.WindowControl(searchDepth=1, ClassName="Window", Name="Intel® Extreme Tuning Utility")
+    print("BEFORE")
+    #window = auto.WindowControl(searchDept=1, Name="Prime95")
+    print("AFTER")
+    if auto.WaitForExist(window, 3):
+        print('FOUND!')
+    else:
+        print('NOT!')
+    #exit(0)
+    print(window)
+    print("HELL")
+    edit = window.ButtonControl(searchDepth=2, ClassName="Button", AutomationId="Navigation:StressTesting")
+    edit.Click()
+
+    # when calling SendKeys, uiautomation starts to search window and edit in 15 seconds
+    # because SendKeys indirectly calls Control.Element and Control.Element is None
+    # if window and edit don't exist in 15 seconds, a LookupError exception will be raised
+    # try:
+    #     edit.SendKeys('first notepad')
+    # except LookupError as ex:
+    #     print("The first notepad doesn't exist in 15 seconds")
+    #     return
+    # # the second call to SendKeys doesn't trigger a search, the previous call makes sure that Control.Element is valid
+    # edit.SendKeys('{Ctrl}a{Del}')
+    # window.GetWindowPattern().Close()  # close the first Notepad, window and edit become invalid even though their Elements have a value
+
+    # subprocess.Popen('notepad.exe')  # run second Notepad
+    # window.Refind()  # need to refind window, trigger a new search
+    # edit.Refind()  # need to refind edit, trigger a new search
+    # edit.SendKeys('second notepad')
+    # edit.SendKeys('{Ctrl}a{Del}')
+    # window.GetWindowPattern().Close()  # close the second Notepad, window and edit become invalid again
+
+    # subprocess.Popen('notepad.exe')  # run third Notepad
+    # if window.Exists(3, 1): # trigger a new search
+    #     if edit.Exists(3):  # trigger a new search
+    #         edit.SendKeys('third notepad')  # edit.Exists makes sure that edit.Element has a valid value now
+    #         edit.SendKeys('{Ctrl}a{Del}')
+    #     window.GetWindowPattern().Close()
+    # else:
+    #     print("The third notepad doesn't exist in 3 seconds")
+
+
+if __name__ == '__main__':
+    main()
