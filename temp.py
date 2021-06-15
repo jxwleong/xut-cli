a928ca6614bdf203e362e70d0e6682fc31a05331
jxwleong <jason.xie.wei.leong@gmail.com>
2021-06-15 20:28:59 +0800

    Added temporary code to get system version on XUT using uiautomation

--- temp.py
+++ temp.py
@@ -6,6 +6,56 @@ auto.uiautomation.SetGlobalSearchTimeout(15)  # set new timeout 15
 
 xtu_exe = r"C:\Program Files\Intel\Intel(R) Extreme Tuning Utility\Client\XtuUiLauncher.exe"
 prime95=r"G:\MyProjects\xprime95\p95v303b6.win32\prime95.exe"
+
+SysInfo = {
+    # PROCESSOR
+    "brand_string": "SystemInfo:ProcessorBrandString",
+    "family_string": "SystemInfo:ProcessorFamilyString",
+    "physical_core": "SystemInfo:ProcessorPhysicalCpuCores",
+    "logical_core": "SystemInfo:ProcessorLogicalCpuCores" ,
+    "possible_turbo_bins": "SystemInfo:ProcessorOverclockableTurboBins",
+    "turbo_overclockable": "SystemInfo:ProcessorTurboOverclockable",
+    "feature_flags": "SystemInfo:ProcessorFeatureFlags",
+    "instructions": "SystemInfo:ProcessorInstructions",
+    "intel_turbo_boost_max": "SystemInfo:ProcessorFavoredCoreSupported",
+    "intel_speed_shift": "SystemInfo:ProcessorHwpEnabled",
+    "microcode_update": "SystemInfo:ProcessorMicrocodeUpdate",
+    "hybrid_core": "SystemInfo:ProcessorIsHybridArchitecture",            
+
+    # Graphics
+    "graphic_0": "SystemInfo:GraphicsName0",
+    "gfx0_compatibility": "SystemInfo:GraphicsCompatibility0",
+    "gfx0_ram": "SystemInfo:GraphicsRAM0",
+    "gfx0_dac": "SystemInfo:GraphicsDACType0",
+    "gfx0_driver_version": "SystemInfo:GraphicsDriverVersion0",
+    "gfx0_driver_date": "SystemInfo:GraphicsDriverDate0",
+
+    "graphic_1": "SystemInfo:GraphicsName1",
+    "gfx1_compatibility": "SystemInfo:GraphicsCompatibility1",
+    "gfx1_ram": "SystemInfo:GraphicsRAM1", 
+    "gfx1_dac": "SystemInfo:GraphicsDACType1",
+    "gfx1_driver_version": "SystemInfo:GraphicsDriverVersion1",
+    "gfx1_driver_date": "SystemInfo:GraphicsDriverDate1",
+
+    # Operating System
+    "os_manufacturer": "SystemInfo:OperatingSystemManufacturer",
+    "os_name": "SystemInfo:OperatingSystemName",
+    "os_version": "SystemInfo:OperatingSystemVersion",
+    "os_service_pack": "SystemInfo:OperatingSystemServicePack",
+    "os_system_name": "SystemInfo:OperatingSystemSystemName",
+    "os_boot_device": "SystemInfo:OperatingSystemBootDevice",
+
+    # Watchdog
+    "watchdog_support": "SystemInfo:WatchdogWatchdogPresent",
+    "watchdog_run_at_boot": "SystemInfo:WatchdogRunningAtBoot",
+    "watchdog_failed": "SystemInfo:WatchdogFailed",
+
+    # Memory
+    #"memory_installed": "SystemInfo:MemoryTotalInstalledMemory",
+    #"memory_bank0": "SystemInfo:MemoryBankLabel0",
+    #"memory_
+}
+
 def main():
     proc = subprocess.Popen(xtu_exe)
     window = auto.WindowControl(searchDepth=1, ClassName="Window", Name="Intel® Extreme Tuning Utility")
@@ -19,31 +69,43 @@ def main():
     #exit(0)
     print(window)
     print("HELL")
-    edit = window.ButtonControl(searchDepth=2, ClassName="Button", AutomationId="Navigation:StressTesting")
-    print(type(edit))
-    edit.Click()
-    
-    checkbox = window.CheckBoxControl(searchDepth=3, ClassName="CheckBox", AutomationId="StressTestSelected:CpuTest")
-    #edit.Click()
-    print(type(checkbox))
-    print(checkbox)
-    #print(checkbox.GetTogglePattern())
-    checkbox.GetTogglePattern().Toggle()
 
-    start = window.ButtonControl(searchDepth=3, ClassName="Button", AutomationId="StressTestStartButton")
-    print(type(start))
-    start.Click()
-    
-    time.sleep(5)
+    for key, value in SysInfo.items():
+        text = window.TextControl(searchDepth=3, ClassName="TextBlock", AutomationId=value)
+        print(f"{key}: {text.Name}")
 
-    end = window.ButtonControl(searchDepth=3, ClassName="Button", AutomationId="StressTestStopButton")
-    print(type(end))
-    end.Click()
+    text = window.TextControl(searchDepth=3, ClassName="TextBlock", AutomationId="SystemInfo:ProcessorBrandString")
+    print(text)
+    print(type(text))
+    print(text.Name)
+
+    text = window.TextControl(searchDepth=3, ClassName="TextBlock", AutomationId="SystemInfo:ProcessorFamilyString")
+    print(text)
+    print(type(text))
+    print(text.Name)    
+    # edit = window.ButtonControl(searchDepth=2, ClassName="Button", AutomationId="Navigation:StressTesting")
+    # print(type(edit))
+    # edit.Click()
+    
+    # checkbox = window.CheckBoxControl(searchDepth=3, ClassName="CheckBox", AutomationId="StressTestSelected:CpuTest")
+    # print(type(checkbox))
+    # print(checkbox)
+    # checkbox.GetTogglePattern().Toggle()
+
+    # start = window.ButtonControl(searchDepth=3, ClassName="Button", AutomationId="StressTestStartButton")
+    # print(type(start))
+    # start.Click()
+    
+    # time.sleep(5)
+
+    # end = window.ButtonControl(searchDepth=3, ClassName="Button", AutomationId="StressTestStopButton")
+    # print(type(end))
+    # end.Click()
 
     #proc.terminate()
-    subprocess.call("taskkill /f /im XtuUiLauncher.exe")
-    subprocess.call("taskkill /f /im XtuService.exe")
-    subprocess.call("taskkill /f /im PerfTune.exe")
+    # subprocess.call("taskkill /f /im XtuUiLauncher.exe")
+    # subprocess.call("taskkill /f /im XtuService.exe")
+    # subprocess.call("taskkill /f /im PerfTune.exe")
     #edit.GetValuePattern().SetValue('Hello')
     # when calling SendKeys, uiautomation starts to search window and edit in 15 seconds
     # because SendKeys indirectly calls Control.Element and Control.Element is None
